c4abfb57a8fd76d65d274c2c6d244d807d814023
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-05-07 22:54:12 +0300

    task-1 first try

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,33 @@
 import css from './Profile.module.css'
 
-function Profile() {
-    
+function Profile( { name, tag, location, image, stats }) {
+   <div>
+  <div>
+    <img
+      src={image}
+      alt={name}
+    />
+    <p>{name}</p>
+    <p>@{tag}</p>
+    <p>{location}</p>
+  </div>
+
+  <ul>
+    <li>
+      <span>Followers</span>
+      <span>{stats.followers}</span>
+    </li>
+    <li>
+      <span>Views</span>
+      <span>{stats.views}</span>
+    </li>
+    <li>
+      <span>Likes</span>
+      <span>{stats.likes}</span>
+    </li>
+  </ul>
+</div>
+ 
 }
 
 export default Profile;
