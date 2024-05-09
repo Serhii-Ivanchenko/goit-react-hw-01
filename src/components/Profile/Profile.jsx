bcc9a4c5569836971de5417c42b36512c1ae6c83
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-05-09 21:06:01 +0300

    add task-1 styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,26 +2,26 @@ import css from './Profile.module.css';
 
 function Profile({ name, tag, location, image, stats }) {
   return (
-    <div>
-      <div>
-        <img src={image} alt={name} />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div className={css.card}>
+      <div className={css.personalInfo}>
+        <img className={css.avatar} src={image} alt={name} />
+        <p className={css.name}>{name}</p>
+        <p className={css.info}>@{tag}</p>
+        <p className={css.info}>{location}</p>
       </div>
 
-      <ul>
-        <li>
+      <ul className={css.list}>
+        <li className={css.listItem}>
           <span>Followers</span>
-          <span>{stats.followers}</span>
+          <span className={css.figures}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={css.listItem}>
           <span>Views</span>
-          <span>{stats.views}</span>
+          <span className={css.figures}>{stats.views}</span>
         </li>
-        <li>
+        <li className={css.listItem}>
           <span>Likes</span>
-          <span>{stats.likes}</span>
+          <span className={css.figures}>{stats.likes}</span>
         </li>
       </ul>
     </div>
